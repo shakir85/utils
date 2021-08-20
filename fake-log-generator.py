07bfd5010a291612c8cd9a71eca392320a47024f
Shakir <35549045+shakir85@users.noreply.github.com>
2021-08-20 10:47:13 -0500

    Create fake-log-generator.py

---
+++ fake-log-generator.py
@@ -0,0 +1,13 @@
+import random
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s-%(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename="foo.log", filemode='a')
+
+for i in range(0, 45):
+    r = random.randint(0, 2)
+    if r == 0:
+        logging.warning('Test warning log')
+    elif r == 1:
+        logging.critical('Test critical log')
+    else:
+        logging.error('Test error log')
