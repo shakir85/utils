48412bb2b2cea2def6e5f01dc7bfcdc88965a3db
Shakir <35549045+shakir85@users.noreply.github.com>
2021-09-23 20:58:10 -0500

    Update clean-containers-delete.sh

--- clean-containers-delete.sh
+++ clean-containers-delete.sh
@@ -8,12 +8,18 @@ docker container stop $container
 docker container rm $container
 
 echo "Deleting dangling images & volumes (all)..."
-docker volume rm $(docker volume ls -qf dangling=true)
+CHECK_VOL=$(docker volume ls -qf dangling=true)
 
-CHECK=$(docker volume ls -qf dangling=true)
+if [[ ! -z $CHECK_VOL ]] ; then
+    docker volume rm $(docker volume ls -qf dangling=true)
+    else
+    echo "No dangling volumes."
+fi
+
+CHECK_IMG=$(docker volume ls -qf dangling=true)
 
 if [[ ! -z $CHECK ]] ; then
-    docker image rm $(docker volume ls -qf dangling=true)
+    docker image rm $(docker image ls -qf dangling=true)
     else
     echo "No dangling images."
 fi
