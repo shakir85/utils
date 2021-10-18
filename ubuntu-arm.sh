d7959358aa9cff7379d689e225f4e3985b0d91b9
Shakir <35549045+shakir85@users.noreply.github.com>
2021-10-17 20:18:32 -0500

    Create ubuntu-arm.sh

---
+++ ubuntu-arm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### --------------------------------------------------------------
+### Fix Ubuntu arm64 when struggles to get an IPv4 + other things
+### --------------------------------------------------------------
+##
+## Set ipv4, default gw, and dns (install ifconfig via net-tools)
+##
+# set ipv4
+ifconfig eth0 192.168.1.119 netmask 255.255.255.0 up
+
+# set dns server to pihole
+echo "nameserver 192.168.1.xx" > /etc/resolv.conf
+
+# set default gateway
+route add default gw 192.168.1.1
+
+# test
+ping -c5 google.com
+
+## Take down an interface
+# ip addr show
+# ip link set eth0 up
+# ip link set eth0 down
+## Show current routing or edit /etc/netplan/*.yaml & run `netplan apply`
+# ip route show
