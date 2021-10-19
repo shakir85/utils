10e845db2efe4c520c54b97c4d92c98637c322a4
Shakir <35549045+shakir85@users.noreply.github.com>
2021-10-18 20:31:40 -0500

    Update nfs-mount.sh

--- nfs-mount.sh
+++ nfs-mount.sh
@@ -3,13 +3,14 @@
 # Mount NFS folder to fstadb
 
 read -r "Enter NAS IP address..." IP
-read -r "Enter NAS mount absolute path..." NAS_Vol
-read -r "Enter local mount directory absolute path..." Local_Vol
+read -r "Enter NAS mounting point's absolute path..." NAS_Vol
+read -r "Enter local mounting point absolute path..." Local_Vol
 
 
 if ! grep -q "$Local_Vol" /etc/fstab ; 
     then
         echo "# Custom NAS Volume" >> /etc/fstab
+        #  $NASiP:/volume1/nfs-data /mnt/nfs-data nfs nouser,rsize=8192,wsize=8192,atime,auto,rw,dev,exec,suid 0 0
         echo "$IP:$NAS_Vol $Local_Vol nfs nouser,rsize=8192,wsize=8192,atime,auto,rw,dev,exec,suid 0 0" >> /etc/fstab
         mount -a
     else
