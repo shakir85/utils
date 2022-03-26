4ce4686ec686126b97c27c0261a29af1a872e630
Shakir <35549045+shakir85@users.noreply.github.com>
2022-03-25 21:00:35 -0500

    Create watch_cluster.sh

---
+++ watch_cluster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+watch -n 0.5 "kubectl config current-context; echo ''; kubectl config view | grep namespace; echo ''; kubectl get namespace,node,ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide"
