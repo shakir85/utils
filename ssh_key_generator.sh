77384a2ce1cf1891b16a7e269f99257b21ca81b6
Shakir <35549045+shakir85@users.noreply.github.com>
2022-09-23 22:08:11 -0500

    Update ssh_key_generator.sh

--- ssh_key_generator.sh
+++ ssh_key_generator.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Script to generate ssh keys.
+# Usage:
+#   1. Run this script in the destination server, supply a meaningful hostname and username.
+#   2. Copy the generate public key (<foo>.pub) into the server's ~/.ssh/authorized_keys
+#   3. Copy the generated private key into your machine (or to 1Password to share it with the team)
+#   4. Delete the keys from the server. No need to keep them there.
+#   5. Now you can use the generate private key to ssh to the server. Chmod it 400 (or 644) first.
+
+
 # Set bash strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
+
 
 # Get the full directory path regardless where this script is being called from
 # Taken from: https://stackoverflow.com/a/246128
@@ -19,7 +29,7 @@ read -r username
 ssh-keygen -t rsa -b 2048 -C "$username@$hostname" -q -N "" -f "$SCRIPT_DIR/sshkey-$username@$hostname-$RANDOM_STRING"
 EXIT_CODE=$?
 
-if [ $EXIT_CODE -ne 0 ]; then 
+if [ $EXIT_CODE -ne 0 ]; then
     echo "Error"
 else
     echo "Done."
