ef846bdda94961f26b2d83fee4ae901fc0453f13
Shakir <35549045+shakir85@users.noreply.github.com>
2023-06-15 09:02:49 -0700

    Create center_msg.sh

---
+++ center_msg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Function takes string input and print it in the center
+# of the terminal window with lines on the sides of the test.
+# Example:
+# __________________hello worl_________________
+#
+
+print_center() {
+  termwidth="$(tput cols)"
+  padding="$(printf '%0.1s' -{1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+print_center "info message goes here"
+
