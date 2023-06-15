15f046054b1006f6db294412fb1a99710ed5dbc9
Shakir <35549045+shakir85@users.noreply.github.com>
2023-06-15 09:07:13 -0700

    Update center_msg.sh

--- center_msg.sh
+++ center_msg.sh
@@ -11,5 +11,5 @@ print_center() {
   padding="$(printf '%0.1s' -{1..500})"
   printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
 }
-print_center "info message goes here"
+# print_center "info message goes here"
 
