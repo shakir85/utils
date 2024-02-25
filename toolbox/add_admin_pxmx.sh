a20265c642561f7ebf6cf4c79718d162e87d429b
Shakir Almasaari <sralmasaari@gmail.com>
2024-02-24 22:53:06 -0800

    Create add_admin_pxmx.sh

---
+++ toolbox/add_admin_pxmx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copied from https://registry.terraform.io/providers/Telmate/proxmox/latest/docs
+# Create admin role, user, and attaches the admin ACL the new admin user.
+pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
+pveum user add terraform-prov@pve --password <password>
+pveum aclmod / -user terraform-prov@pve -role TerraformProv
